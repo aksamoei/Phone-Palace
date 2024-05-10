441476e0ba248f373a78de850ac61f3f98957400
Abraham <abukip344@gmail.com>
2024-05-10 06:03:07 -0400

    finalized filter

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,11 +6,18 @@ import NavBar from '../components/NavBar';
 
 function Home({ phones}) {
     const [searchQuery, setSearchQuery] = useState('');
+    const [filterBrand, setFilterBrand] = useState("Select Brand");
+
+
+    function handleBrandFilter(event){
+      setFilterBrand(event.target.value);
+    }
     
 
    
     const filteredPhones = phones.filter(phone =>
-      phone.phoneName.toLowerCase().includes(searchQuery.toLowerCase())
+      phone.phoneName.toLowerCase().includes(searchQuery.toLowerCase()) &&
+      (filterBrand === "Select Brand" || phone.brand === filterBrand)
     );
   
     const handleSearchChange = (event) => {
@@ -54,6 +61,23 @@ function Home({ phones}) {
           className="search-input"
         />
         <FaSearch className="search-icon" /> {/* Search icon */}
+        <select id="brandSelect" value={filterBrand} onChange={handleBrandFilter}>
+                    <option value="Select Brand">Select Brand</option>
+                    <option value="Apple">Apple</option>
+                    <option value="Samsung">Samsung</option>
+                    <option value="OnePlus">OnePlus</option>
+                    <option value="Google">Google</option>
+                    <option value="Xiaomi">Xiaomi</option>
+                    <option value="Sony">Sony</option>
+                    <option value="Huawei">Huawei</option>
+                    <option value="Motorola">Motorola</option>
+                    <option value="Realme">Realme</option>
+                    <option value="Oppo">Oppo</option>
+                    <option value="Asus">Asus</option>
+                    <option value="Nokia">Nokia</option>
+                    <option value="Lenovo">Lenovo</option>
+                    <option value="BlackBerry">BlackBerry</option>
+                </select>
       </div>
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         {filteredPhones.map(phone => (
